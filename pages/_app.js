927d9f5ec9f93a1240f1e72c91091ecdcb0e04c4
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-08-25 09:53:53 -0400

    add tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Footer from '../components/layout/Footer'
+import Header from '../components/layout/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>  
+    <Header/>
+    <Component {...pageProps} />
+    <Footer/>
+    </>
+  )
 }
 
 export default MyApp

05374618c1f9b28f59ee55a80734dd11d8318aa5
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-08-26 23:44:01 -0400

    Add google analytics

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension:/\.mdx?$/,
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  withMDX:['js','jsx','mdx']
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-import Head from 'next/head'
-import Header from '../components/layout/Header'
+import Head from 'next/head';
+import fs from 'fs';
+import path from 'path';
+import React, { useEffect, useState } from 'react';
+import matter from 'gray-matter';
+import Post from '../components/Post';
 
-
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts)
   return (
     <div>
-    BODY
+      {posts.map((post, index) => {
+        return <Post key={index} post={post} />;
+      })}
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // get files from markdown
+  const files = fs.readdirSync(path.join('markdown'));
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(
+      path.join('markdown', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+  return {
+    props: { posts },
+  };
 }

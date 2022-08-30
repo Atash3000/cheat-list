300b9bf09225770327a5afcc3d658e8ebb728775
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-08-30 01:15:59 -0400

    Remove jombo

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -7,7 +7,7 @@ function MainContainer({posts}) {
     <section className='bg-blue dark:bg-primary w-full flex flex-coltext-center pt-14'>
       <div className='max-container lg:w-3/4'>
         <h1 className='text-4xl md:text-5xl text-white dark:text-white-dark leading-relaxed text-center'>
-          Cheat list by Atamurad
+          Cheat list by Atamurad X
         </h1>
         <p className='text-xl md:text-2xl text-white dark:text-white-dark leading-tight font-extralight tracking-wide mt-6 mb-20 text-center  '>
           Here are some cheatsheets and quick references contributed by open
